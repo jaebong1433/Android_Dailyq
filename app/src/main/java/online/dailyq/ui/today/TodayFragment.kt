f3a69bb905b48f612364a08791bdbca932c90aa9
bong <jaebong1433@hanmail.net>
2023-05-20 16:19:41 +0900

    first commit

---
+++ app/src/main/java/online/dailyq/ui/today/TodayFragment.kt
@@ -0,0 +1,69 @@
+package online.dailyq.ui.today
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.gson.Gson
+import online.dailyq.api.response.HelloWorld
+import online.dailyq.databinding.FragmentTodayBinding
+import online.dailyq.ui.base.BaseFragment
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+import java.text.DateFormat
+import java.util.*
+
+class TodayFragment : BaseFragment() {
+
+    var _binding: FragmentTodayBinding? = null
+    val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentTodayBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Thread {
+            val url = URL("http://10.0.2.2:5000/v1/hello-world")
+
+            val conn = url.openConnection() as HttpURLConnection
+            conn.connectTimeout = 5000
+            conn.readTimeout = 5000
+            conn.requestMethod = "GET"
+            conn.setRequestProperty("Accept", "application/json")
+            conn.connect()
+
+            val reader = BufferedReader(InputStreamReader(conn.inputStream))
+            val body = reader.readText()
+            reader.close()
+            conn.disconnect()
+
+            val gson = Gson()
+            //DateFormat의 getDateInstance 메서드는
+            //스타일과 로케일을 인자로 받아 그에 맞는 DateFormat 객체를 생성
+            val dateFormat = DateFormat.getDateInstance(DateFormat.MEDIUM, Locale.KOREA)
+            val helloWorld = gson.fromJson(body, HelloWorld::class.java)
+
+            activity?.runOnUiThread {
+                binding.date.text = dateFormat.format(helloWorld.date)
+                binding.question.text = helloWorld.message
+            }
+        }.start()
+    }
+
+
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+
+}
