7b968a8cf72bd160a8d8aed05004c5cd418d4e06
bong <jaebong1433@hanmail.net>
2023-05-21 14:07:30 +0900

    오늘의 질문기능 및 답변 수정/생성 기능 구현

--- app/src/main/java/online/dailyq/ui/base/BaseActivity.kt
+++ app/src/main/java/online/dailyq/ui/base/BaseActivity.kt
@@ -2,8 +2,10 @@ package online.dailyq.ui.base
 
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import online.dailyq.api.ApiService
 
 abstract class BaseActivity : AppCompatActivity() {
+    val api: ApiService by lazy { ApiService.getInstance() }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
--- app/src/main/java/online/dailyq/ui/today/TodayFragment.kt
+++ app/src/main/java/online/dailyq/ui/today/TodayFragment.kt
@@ -1,25 +1,39 @@
 package online.dailyq.ui.today
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.google.gson.Gson
-import online.dailyq.api.response.HelloWorld
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.view.isVisible
+import androidx.lifecycle.lifecycleScope
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import kotlinx.coroutines.launch
+import online.dailyq.R
+import online.dailyq.api.response.Question
 import online.dailyq.databinding.FragmentTodayBinding
 import online.dailyq.ui.base.BaseFragment
-import org.json.JSONObject
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.net.HttpURLConnection
-import java.net.URL
-import java.text.DateFormat
-import java.util.*
+import online.dailyq.ui.write.WriteActivity
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 class TodayFragment : BaseFragment() {
 
     var _binding: FragmentTodayBinding? = null
     val binding get() = _binding!!
+
+    var question: Question? = null
+
+    val startForResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                lifecycleScope.launch {
+                    setupAnswer()
+                }
+            }
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -30,40 +44,66 @@ class TodayFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.writeButton.setOnClickListener {
+            startForResult.launch(Intent(requireContext(), WriteActivity::class.java).apply {
+                putExtra(WriteActivity.EXTRA_QID, question!!.id)
+                putExtra(WriteActivity.EXTRA_MODE, WriteActivity.Mode.WRITE)
+            })
+        }
+        binding.editButton.setOnClickListener {
+            startForResult.launch(Intent(requireContext(), WriteActivity::class.java).apply {
+                putExtra(WriteActivity.EXTRA_QID, question!!.id)
+                putExtra(WriteActivity.EXTRA_MODE, WriteActivity.Mode.EDIT)
+            })
+        }
+        binding.deleteButton.setOnClickListener {
+            showDeleteConfirmDialog()
+        }
 
-        Thread {
-            val url = URL("http://10.0.2.2:5000/v1/hello-world")
+        viewLifecycleOwner.lifecycleScope.launch {
+            val questionResponse = api.getQuestion(LocalDate.now())
+            if (questionResponse.isSuccessful) {
+                question = questionResponse.body()!!
 
-            val conn = url.openConnection() as HttpURLConnection
-            conn.connectTimeout = 5000
-            conn.readTimeout = 5000
-            conn.requestMethod = "GET"
-            conn.setRequestProperty("Accept", "application/json")
-            conn.connect()
+                val dateFormatter = DateTimeFormatter.ofPattern("yyyy. M. d.")
 
-            val reader = BufferedReader(InputStreamReader(conn.inputStream))
-            val body = reader.readText()
-            reader.close()
-            conn.disconnect()
+                binding.date.text = dateFormatter.format(question!!.id)
+                binding.question.text = question!!.text
 
-            val gson = Gson()
-            //DateFormat의 getDateInstance 메서드는
-            //스타일과 로케일을 인자로 받아 그에 맞는 DateFormat 객체를 생성
-            val dateFormat = DateFormat.getDateInstance(DateFormat.MEDIUM, Locale.KOREA)
-            val helloWorld = gson.fromJson(body, HelloWorld::class.java)
-
-            activity?.runOnUiThread {
-                binding.date.text = dateFormat.format(helloWorld.date)
-                binding.question.text = helloWorld.message
+                setupAnswer()
             }
-        }.start()
+        }
+
     }
-
-
 
     override fun onDestroyView() {
         _binding = null
         super.onDestroyView()
     }
 
+    fun showDeleteConfirmDialog() {
+        MaterialAlertDialogBuilder(requireContext())
+            .setMessage(R.string.dialog_msg_are_you_sure_to_delete)
+            .setPositiveButton(R.string.ok) { dialog, which ->
+                lifecycleScope.launch {
+                    val deleteResponse = api.deleteAnswer(question!!.id)
+                    if (deleteResponse.isSuccessful) {
+                        binding.answerArea.isVisible = false
+                        binding.writeButton.isVisible = true
+                    }
+                }
+            }.setNegativeButton(R.string.cancel) { dialog, which ->
+
+            }.show()
+    }
+
+    suspend fun setupAnswer() {
+        val question = question ?: return
+
+        val answer = api.getAnswer(question.id).body()
+        binding.answerArea.isVisible = answer != null
+        binding.textAnswer.text = answer?.text
+
+        binding.writeButton.isVisible = answer == null
+    }
 }
